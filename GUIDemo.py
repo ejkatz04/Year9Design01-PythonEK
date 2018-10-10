e0fed254e5d1ff3e20c214198707f8b4b9db1665
Eli Katz <eli.katz@Y22-Design-Eli-Katz.local>
2018-10-10 13:36:02 -0400

    eli push

---
+++ GUIDemo.py
@@ -0,0 +1,11 @@
+#tkinter is a module that holds all the fuction that let us easily
+#make GUI elements
+import tkinter as tk
+
+#creating the main window.
+#To do this we need to call the Tk() function
+root = tk.Tk()
+
+
+
+root.mainloop()
