ac7ff2f9800564896332630b29b1185e95dcbbf9
Eli Katz <eli.katz@Y22-Design-Eli-Katz.local>
2018-11-01 11:59:26 -0400

    DI modified

--- DevelopingIdeasStringDemo1.py
+++ DevelopingIdeasStringDemo1.py
@@ -3,7 +3,7 @@
 #Step 1 take input for first name
 fName = input("Input First Name: ")
 #Step 2 take input for last name
-lname = input("Input Last Name: ")
+lName = input("Input Last Name: ")
 
 #PROCESS
 userName = fName[0] + lName
