273443febe173a1b1771af3384a8baf796a927e7
Eli Katz <eli.katz@Y22-Design-Eli-Katz.local>
2018-11-01 11:56:19 -0400

    DI added

---
+++ DevelopingIdeasStringDemo1.py
@@ -0,0 +1,12 @@
+#INPUT
+
+#Step 1 take input for first name
+fName = input("Input First Name: ")
+#Step 2 take input for last name
+lname = input("Input Last Name: ")
+
+#PROCESS
+userName = fName[0] + lName
+
+#OUTPUT
+print(userName)
